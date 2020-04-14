b4e005184c899f5283ce33c0e2a5d638c4f0ebf8
johanCas320 <61796945+johanCas320@users.noreply.github.com>
2020-04-13 21:38:24 -0500

    Update BCDtoSSeg_labsland.v

--- src/BCDtoSSeg_labsland.v
+++ src/BCDtoSSeg_labsland.v
@@ -1,18 +1,21 @@
 module bcd2sseg (V_SW, G_HEX0);
-
+// Archivo de prueba usado para LabsLand dado a que usan una FPGA diferente
+	//su sistema exige ciertos cambios como la definicion de las entradas y salidas
    input wire [3:0] V_SW;
    output wire [6:0] G_HEX0;
 
     reg [6:0] SSeg;
     wire [3:0]  BCD;
-    
+    // asigna el nombre de las entradas y salidas al nmbre correspondiente de los cables en el archivo de la FPGA.
     assign BCD =V_SW;
     assign G_HEX0 = SSeg;
 
 
 always @ ( * ) begin
   case (BCD)
-    4'b0000: SSeg = 7'b1000000; // "0"  
+// se cambiaron(se invirtieron) los numeros binaros con respecto al primer archivo
+// dado que el 7Seg en este es anodo común se mostraban los numeros y las letras al reves.
+    	4'b0000: SSeg = 7'b1000000; // "0"  
 	4'b0001: SSeg = 7'b1111001; // "1" 
 	4'b0010: SSeg = 7'b0100100; // "2" 
 	4'b0011: SSeg = 7'b0110000; // "3" 
